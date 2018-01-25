9a14a56c73bfc8b54a19408d75c0dd9d529c2e4e
Matt Carmody <carmodym2@gmail.com>
2018-01-25 17:06:51 -0600

    Move update_HackerRank to separate file

---
+++ hackerRank.py
@@ -0,0 +1,40 @@
+# Component of trackr.py - saves data from HackerRank.com
+
+import bs4
+import datetime
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def update_HackerRank(wb):
+    url = "https://www.hackerrank.com/{USERNAME}"
+
+    browser = webdriver.Firefox()
+    browser.get(url)
+    loginElem = browser.find_element_by_class_name("login").click()
+
+    username = browser.find_element_by_name("login")
+    username.send_keys("{EMAIL}")
+
+    password = browser.find_element_by_name("password")
+    password.send_keys("{PASSWORD}")
+    password.send_keys(Keys.RETURN)
+
+    hackosUrl = "https://www.hackerrank.com/{USERNAME}/hackos"
+    browser.get(hackosUrl)
+    browser.refresh()
+
+    innerHTML = browser.execute_script("return document.body.innerHTML")
+    bsSoup = bs4.BeautifulSoup(innerHTML, "lxml")
+    h3 = bsSoup.find("h3")
+
+    hackosRegex = re.compile(r"Total Hackos: (\d*)")
+    mo = hackosRegex.search(h3.text)
+    hackos = int(mo.group(1))
+
+    hrSheet = wb.get_sheet_by_name("HackerRank")
+    newRow = hrSheet.max_row + 1
+    hrSheet["A" + str(newRow)] = datetime.date.today().strftime("%B %d, %Y")
+    hrSheet["B" + str(newRow)] = hackos
+    
+    return wb
