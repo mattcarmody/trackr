e91a23541d3776afc1b5ae54dcd5b8be4999f23f
Matt Carmody <33763384+mattcarmody@users.noreply.github.com>
2018-01-25 16:06:20 -0600

    Update trackr.py
    Add HackerRank (using Selenium)

--- trackr.py
+++ trackr.py
@@ -5,7 +5,10 @@ import bs4
 import datetime
 import json
 import openpyxl
+import re
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 def update_Duolingo(wb):
     
@@ -73,12 +76,47 @@ def update_Goodreads(wb):
     grSheet["B" + str(newRow)] = count
     
     return wb
+
+def update_HackerRank(wb):
+    url = "https://www.hackerrank.com/{USERNAME}"
+
+    browser = webdriver.Firefox()
+    browser.get(url)
+    loginElem = browser.find_element_by_class_name("login").click()
+
+    username = browser.find_element_by_name("login")
+    username.send_keys("{EMAIL}")
+
+    password = browser.find_element_by_name("password")
+    password.send_keys("{PASSWORD}")
+    password.send_keys(Keys.RETURN)
+
+    hackosUrl = "https://www.hackerrank.com/{USERNAME}/hackos"
+    browser.get(hackosUrl)
+    browser.refresh()
+
+    innerHTML = browser.execute_script("return document.body.innerHTML")
+    bsSoup = bs4.BeautifulSoup(innerHTML, "lxml")
+    h3 = bsSoup.find("h3")
+
+    hackosRegex = re.compile(r"Total Hackos: (\d*)")
+    mo = hackosRegex.search(h3.text)
+    hackos = int(mo.group(1))
+
+    hrSheet = wb.get_sheet_by_name("HackerRank")
+    newRow = hrSheet.max_row + 1
+    hrSheet["A" + str(newRow)] = datetime.date.today().strftime("%B %d, %Y")
+    hrSheet["B" + str(newRow)] = hackos
+    
+    return wb
+
 def main():
     wb = openpyxl.load_workbook('trackr.xlsx')      
     wb = update_Duolingo(wb)
     wb = update_Codewars(wb)
     wb = update_Chess(wb)
     wb = update_Goodreads(wb)
+    wb = update_HackerRank(wb)
     wb.save('trackr.xlsx')
     
 main()
