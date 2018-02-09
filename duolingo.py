6befbc45f40bc124a4848179df7e1e8937557b4e
Matt Carmody <carmodym2@gmail.com>
2018-02-09 12:57:39 -0600

    Remove commented out xlsx block

--- duolingo.py
+++ duolingo.py
@@ -30,16 +30,4 @@ def update_Duolingo(cur):
     else:
         print("Duolingo was already updated today.")
     
-    '''    
-    duoSheet = wb.get_sheet_by_name("Duolingo")
-    newRow = duoSheet.max_row + 1
-    
-    if not duoSheet["A{}".format(newRow-1)].value == today:
-        duoSheet["A{}".format(newRow)] = today
-        for i in range(len(duoData["languages"])):
-            duoSheet["{}{}".format(openpyxl.utils.get_column_letter(i+2), newRow)] = duoData["languages"][i]["points"]
-        print("Updated Duolingo.")
-    else:
-        print("Duolingo was already updated today.")
-    '''
     return cur
