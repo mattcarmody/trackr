c3a2cfb14bbbb4b7fd16de074b871f475bd9cce7
Matt Carmody <carmodym2@gmail.com>
2018-03-12 14:30:41 -0600

    Move imports and update function calls to loops. General clean up.

--- trackr.py
+++ trackr.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-# trackr.py - Automate record keeping for sites I frequent and provide
+# trackr.py - Automate record keeping for sites I frequent and provide the
 # manager part of myself with tracking of the worker part of myself.
 
 import datetime
+import importlib
 import logging
 logging.basicConfig(filename='trackr_log.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 import sqlite3
 import sys
 import traceback
 
-from hackerrank import update_hackerrank
-from duolingo import update_duolingo
-from codewars import update_codewars
-from chess import update_chess
-from goodreads import update_goodreads
-from my_email import update_my_email
-from track_warmup import update_track_warmup
+# Import trackr update modules. (Add new in both modules and functions)
+modules = ["duolingo", "codewars", "chess", "goodreads", "my_email", "track_warmup"]
+gbl = globals()
+for module in modules:
+    gbl[module] = importlib.import_module(module)    
+functions = [duolingo.update_duolingo, codewars.update_codewars, chess.update_chess, goodreads.update_goodreads, my_email.update_my_email, track_warmup.update_track_warmup]
 
 import visuals_body
 import visuals_duolingo
@@ -33,60 +33,29 @@ def main():
     with conn:     
         cur = conn.cursor()
         
-        try:
-            cur = update_duolingo(cur)
-        except:
-            with open("trackr_log.txt", "a") as error_file:
-                error_file.write(traceback.format_exc())
-            print("Duolingo failed...")
-        try:
-            cur = update_codewars(cur)
-        except:
-            with open("trackr_log.txt", "a") as error_file:
-                error_file.write(traceback.format_exc())
-            print("Codewars failed...")
-        try:
-            cur = update_chess(cur)
-        except:
-            with open("trackr_log.txt", "a") as error_file:
-                error_file.write(traceback.format_exc())
-            print("Chess failed...")
-        try:
-            cur = update_goodreads(cur)
-        except:
-            with open("trackr_log.txt", "a") as error_file:
-                error_file.write(traceback.format_exc())
-            print("Goodreads failed...")
-        #try:
-        #    cur = update_hackerrank(cur)
-        #except:
-            #with open("trackr_log.txt", "a") as error_file:
-                #error_file.write(traceback.format_exc())    
-            #print("HackerRank failed...")
-        try:
-            cur = update_my_email(cur)
-        except:
-            with open("trackr_log.txt", "a") as error_file:
-                error_file.write(traceback.format_exc())
-            print("Email update failed...")
-        try:
-            cur = update_track_warmup(cur)
-        except:
-            with open("trackr_log.txt", "a") as error_file:
-                error_file.write(traceback.format_exc())
-            print("There was a problem updating warmup.")
+        # Update functions
+        for f in functions:
+            try:
+                cur = f(cur)
+            except:
+                with open("trackr_log.txt", "a") as error_file:
+                    error_file.write(traceback.format_exc())
+                print(str(f) + " failed...")
+        
+        # Visualizations
         try:
             today = datetime.date.today()
             day_of_year = datetime.date.today().timetuple().tm_yday
                 
-            # Bifortly
+            # Bifortly (every 4 Sundays)
             if today.weekday() == REVIEW_DOW and ((day_of_year - BIFORT_START + 1) // 7) % 4 == 0:
                 visuals_duolingo.duolingo_bifortly_visuals(cur)
                 
             # Weekly
-            #if today.weekday() == REVIEW_DOW:
+            if today.weekday() == REVIEW_DOW:
+                pass
             
-            # Daily
+            # Each call
             if True:
                 visuals_duolingo.duolingo_weekly_visuals(cur)
                 visuals_body.body_week_visuals(cur)
@@ -95,6 +64,7 @@ def main():
             with open("trackr_log.txt", "a") as error_file:
                 error_file.write(traceback.format_exc())
             print("Visualizations failed...")
+        
     logging.debug("Finish main.")
         
 if __name__ == "__main__":
