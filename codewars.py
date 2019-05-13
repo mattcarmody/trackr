b5c573fb2e4dc05558f15b6ebf6e8bd9068278c2
Matt Carmody <carmodym2@gmail.com>
2019-05-13 20:58:37 -0300

    Refactor codewars and remove unnecessary requests

--- codewars.py
+++ codewars.py
@@ -7,26 +7,26 @@ import requests
 from getDate import get_date
 import personal
 
+def scrape_codewars():
+	cwUrl = "https://www.codewars.com/api/v1/users/{}".format(personal.data["cwUsername"])
+	cwResponse = requests.get(cwUrl, auth=("?access_key", personal.data["cwAccessKey"]))
+	cwResponse.raise_for_status()
+	return json.loads(cwResponse.text)
+
 def update_codewars(cur):
-    # Pull and load JSON data
-    cwUrl = "https://www.codewars.com/api/v1/users/{}".format(personal.data["cwUsername"])
-    cwResponse = requests.get(cwUrl, auth=("?access_key", personal.data["cwAccessKey"]))
-    cwResponse.raise_for_status()
-    cwData = json.loads(cwResponse.text)
-    
-    # Select most recent entry
-    cur.execute("SELECT Date FROM codewars ORDER BY Date DESC LIMIT 1")
-    last_entry = cur.fetchone()
-    
-    # If last entry is not today, add new data. Else skip.
-    today = get_date()
-    if today != last_entry[0]:
-        sql = ''' INSERT INTO codewars(Date, Honor, Points, Challenges) VALUES (?,?,?,?)'''
-        new_entry = [today, cwData["honor"], cwData["ranks"]["overall"]["score"], cwData["codeChallenges"]["totalCompleted"]]
-        cur.execute(sql, new_entry[0:4])
-        print("New data added to Codewars.")
-    else:
-        print("Codewars was already updated today.")
-    
-    return cur
-    
+	# Select most recent entry
+	cur.execute("SELECT Date FROM codewars ORDER BY Date DESC LIMIT 1")
+	last_entry = cur.fetchone()
+	
+	# If last entry is not today, add new data. Else skip.
+	today = get_date()
+	if today != last_entry[0]:
+		cwData = scrape_codewars()
+		sql = ''' INSERT INTO codewars(Date, Honor, Points, Challenges) VALUES (?,?,?,?)'''
+		new_entry = [today, cwData["honor"], cwData["ranks"]["overall"]["score"], cwData["codeChallenges"]["totalCompleted"]]
+		cur.execute(sql, new_entry[0:4])
+		print("New data added to Codewars.")
+	else:
+		print("Codewars was already updated today.")
+	return cur
+	
