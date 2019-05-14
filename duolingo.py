39e965d8419d8a279fc1e196495c51553e2d3aa9
Matt Carmody <carmodym2@gmail.com>
2019-05-13 21:00:49 -0300

    Refactor duolingo and remove unnecessary requests

--- duolingo.py
+++ duolingo.py
@@ -7,27 +7,27 @@ import requests
 from getDate import get_date
 import personal
 
+def scrape_duolingo():
+	duoUrl = "https://www.duolingo.com/users/{}".format(personal.data["duoUsername"])
+	duoResponse = requests.get(duoUrl)
+	duoResponse.raise_for_status()
+	return json.loads(duoResponse.text)
+
 def update_duolingo(cur):    
-    # Pull and load JSON data
-    duoUrl = "https://www.duolingo.com/users/{}".format(personal.data["duoUsername"])
-    duoResponse = requests.get(duoUrl)
-    duoResponse.raise_for_status()
-    duoData = json.loads(duoResponse.text)
-    
-    # Select most recent entry
-    cur.execute("SELECT Date FROM duolingo ORDER BY Date DESC LIMIT 1")
-    last_entry = cur.fetchone()
-    
-    # If last entry is not today, add new data. Else skip.
-    today = get_date()
-    if today != last_entry[0]:
-        sql = ''' INSERT INTO duolingo(Date, Greek, Esperanto, Vietnamese, Italian, Welsh, Irish, Czech, Indonesian, Spanish, Chinese, Russian, Portuguese, Norwegian, Turkish, Romanian, Polish, Dutch, French, German, HighValyrian, Korean, Danish, Hungarian, Japanese, Hebrew, Swahili, Swedish, Ukrainian) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
-        new_entry = [today]
-        for i in range(len(duoData["languages"])):
-            new_entry.append(duoData["languages"][i]["points"])
-        cur.execute(sql, new_entry[0:29])
-        print("New data added to Duolingo.")
-    else:
-        print("Duolingo was already updated today.")
-    
-    return cur
+	# Select most recent entry
+	cur.execute("SELECT Date FROM duolingo ORDER BY Date DESC LIMIT 1")
+	last_entry = cur.fetchone()
+	
+	# If last entry is not today, add new data. Else skip.
+	today = get_date()
+	if today != last_entry[0]:
+		duoData = scrape_duolingo()
+		sql = ''' INSERT INTO duolingo(Date, Greek, Esperanto, Vietnamese, Italian, Welsh, Irish, Czech, Indonesian, Spanish, Chinese, Russian, Portuguese, Norwegian, Turkish, Romanian, Polish, Dutch, French, German, HighValyrian, Korean, Danish, Hungarian, Japanese, Hebrew, Swahili, Swedish, Ukrainian) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
+		new_entry = [today]
+		for i in range(len(duoData["languages"])):
+			new_entry.append(duoData["languages"][i]["points"])
+		cur.execute(sql, new_entry[0:29])
+		print("New data added to Duolingo.")
+	else:
+		print("Duolingo was already updated today.")
+	return cur
